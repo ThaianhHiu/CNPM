192ba4cb97bc5697fff287c1d9fce6a7046bea72
ThaianhHiu <sinhthaile@gmail.com>
2024-11-23 14:08:15 +0700

    Add header to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import FoodOrderingComponent from "./FoodOrderingComponent";
 function App() {
     return (
         <>
+        <h1>abcjj:ư
+        </h1>
             <FoodOrderingComponent />
         </>
     );
