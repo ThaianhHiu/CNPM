58549117126859f9151c43fff3de76062953b33d
ThaianhHiu <sinhthaile@gmail.com>
2024-11-23 14:08:40 +0700

    Remove unnecessary header from App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import FoodOrderingComponent from "./FoodOrderingComponent";
 function App() {
     return (
         <>
-        <h1>abcjj:ư
-        </h1>
             <FoodOrderingComponent />
         </>
     );
